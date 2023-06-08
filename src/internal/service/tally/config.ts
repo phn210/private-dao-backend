3b56dd606df6a2ae1c98a4c60b90b3c8626312eb
phn210 <phamhonguyen210@gmail.com>
2023-06-08 23:14:19 +0700

    update topic

--- src/internal/service/tally/config.ts
+++ src/internal/service/tally/config.ts
@@ -12,7 +12,7 @@ export var configs:config = {
     StartBlock: 3642910,
     JobIntervalSec: 1800,
     Routers: ["0xb4Dde6Dba767A281ef15bc4c64E8607ceC25420D"],
-    FundTopic: "0x2b9760e8316bd48dbb4ed8659b3b6deda3c7460d13309d8df59a5b4a9692aa64",
+    FundTopic: "0x9a98994924b3f454e430a7e3c884aa145467ff2913f30a4b4c428a25797c7975",
     BlockRangeLimit: 3000,
     BatchSize: 50,
     ConfirmedBlock: 0,
