93ba8ddceb942287061f70e03f3f2641a3876310
phn210 <phamhonguyen210@gmail.com>
2023-06-08 09:03:59 +0700

    update service interval

--- src/internal/service/tally/config.ts
+++ src/internal/service/tally/config.ts
@@ -10,7 +10,7 @@ export interface config {
 
 export var configs:config = {
     StartBlock: 3642910,
-    JobIntervalSec: 6,
+    JobIntervalSec: 1800,
     Routers: ["0x119cA4DBdC5E30749b85A6eDcB3A0C99444e6062"],
     FundTopic: "0x2b9760e8316bd48dbb4ed8659b3b6deda3c7460d13309d8df59a5b4a9692aa64",
     BlockRangeLimit: 3000,
